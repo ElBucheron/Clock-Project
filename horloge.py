24cb719eb83ff92e32b44db49b1c9fd6a543787a
ElBucheron <reyne.ugo@gmail.com>
2020-09-30 20:38:30 +0100

    Center clock and snow

--- horloge.py
+++ horloge.py
@@ -7,6 +7,7 @@ import pendulum
 import math
 import signal
 from random import randrange
+from random import uniform
 from cozy_fire import fire
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
@@ -37,9 +38,11 @@ HUIT = [[default[1],default[1],default[1],default[1],default[1]],[default[1],def
 NEUF = [[default[1],default[1],default[1],default[1],default[1]],[default[1],default[0],default[1],default[0],default[1]],[default[1],default[1],default[1],default[0],default[1]]]
 CHIFFRE = [ZERO,UN,DEUX,TROIS,QUATRE,CINQ,SIX,SEPT,HUIT,NEUF]
 
-TABLEAU = []
-TABLEAUBIS = []
-showing = 'TABLEAU'
+TABLEAU_LEDS = []
+TABLEAU_SNOW = []
+TABLEAU_HORLOGE = []
+TABLEAU_SNOW = []
+showing = 'TABLEAU_HORLOGE'
 
 def hex_to_rgb(value):
     value = value.lstrip('#')
@@ -48,7 +51,7 @@ def hex_to_rgb(value):
 
 def horloge():
     global default
-    global TABLEAU
+    global TABLEAU_HORLOGE
 
     "Affichage de l'heure"
     tz = pendulum.timezone('Europe/Paris')
@@ -70,48 +73,55 @@ def horloge():
     afficheHeure = [minute2, minute1, heure2, heure1]
     #print("Heure:", afficheHeure)
 
-    coord = 14
+    coord = 7
     for k in range(4):
         afficheCiffre = CHIFFRE[afficheHeure[k]]
         x = 0
         y = 0
         for i in range(coord, coord+3):
             for j in range(1, 6):
-                TABLEAU[i][j] = afficheCiffre[x][y]
-                TABLEAUBIS[i][j] = afficheCiffre[x][y]
+                TABLEAU_HORLOGE[i][j] = afficheCiffre[x][y]
+                #TABLEAU_SNOW[i][j] = afficheCiffre[x][y]
                 y = y + 1
             x = x + 1
             y = 0
         if (k == 1):
-            coord = coord + 4
-            TABLEAU[coord][2] = default[1]
-            TABLEAU[coord][4] = default[1]
-            #TABLEAUBIS[coord][2] = 2
-            #TABLEAUBIS[coord][4] = 2
-            coord = coord + 2
+            coord += 4
+            #TABLEAU_HORLOGE[coord][1] = default[1]
+            #TABLEAU_HORLOGE[coord][2] = default[1]
+            #TABLEAU_HORLOGE[coord][4] = default[1]
+            #TABLEAU_HORLOGE[coord][5] = default[1]
+            coord += 1
+            #TABLEAU_HORLOGE[coord][1] = default[1]
+            #TABLEAU_HORLOGE[coord][2] = default[1]
+            #TABLEAU_HORLOGE[coord][4] = default[1]
+            #TABLEAU_HORLOGE[coord][5] = default[1]
+            
+            #TABLEAU_SNOW[coord][2] = 2
+            #TABLEAU_SNOW[coord][4] = 2
+            coord += 2
         else:
-            coord = coord + 4
+            coord += 4
     
-    tableauVersLEDS()
-
 
 def blinking():
     global showing
 
-    if(showing == 'TABLEAU'):
-        showing = 'TABLEAUBIS'
+    if(showing == 'TABLEAU_HORLOGE'):
+        showing = 'TABLEAU_SNOW'
     else:
-        showing = 'TABLEAU'
+        showing = 'TABLEAU_HORLOGE'
     tableauVersLEDS()
 
 
 def tableauVersLEDS():
     global COULEURS
+    global TABLEAU_LEDS
 
-    #if(showing == 'TABLEAU'):
-    tab = TABLEAU
+    #if(showing == 'TABLEAU_HORLOGE'):
+    tab = TABLEAU_LEDS
     #else:
-        #tab = TABLEAUBIS
+        #tab = TABLEAU_SNOW
 
     i = 0
     led = 0
@@ -130,66 +140,72 @@ def tableauVersLEDS():
 
 
 def snow():
-    global TABLEAU
-    global TABLEAUBIS
+    global TABLEAU_HORLOGE
+    global TABLEAU_SNOW
+    global TABLEAU_LEDS
 
-    initTableau()
-    horloge()
-    
+    initTableauLeds()
+
     removeLine = 0
     for i in range(32):
-        if(TABLEAUBIS[i][7] == 0):
+        if(TABLEAU_SNOW[i][7] == default[0]):
             removeLine = removeLine + 1
         for j in reversed(range(1,8)):
-            #if(TABLEAUBIS[i][j] == 0):
-            if(TABLEAUBIS[i][j-1] != 0):
-                TABLEAUBIS[i][j] = TABLEAUBIS[i][j-1]
-                TABLEAUBIS[i][j-1] = 0
+            #if(TABLEAU_SNOW[i][j] == 0):
+            if(TABLEAU_SNOW[i][j-1] != default[0] and TABLEAU_SNOW[i][j] == default[0]):
+                TABLEAU_SNOW[i][j] = TABLEAU_SNOW[i][j-1]
+                TABLEAU_SNOW[i][j-1] = default[0]
     
-    if(removeLine <= 0):
+    if(removeLine <= 3):
         for i in range(32):
             for j in reversed(range(1,8)):
-                TABLEAUBIS[i][j] = TABLEAUBIS[i][j-1]
-            TABLEAUBIS[i][0] = 0
-    
+                TABLEAU_SNOW[i][j] = TABLEAU_SNOW[i][j-1]
+            TABLEAU_SNOW[i][0] = default[0]
     
     if(randrange(100) < 5):
         num = randrange(32)
-        TABLEAUBIS[num][0] = 6
+        TABLEAU_SNOW[num][0] = "#00C9FF"
     
     for i in range(32):
         for j in range(8):
-            if(TABLEAU[i][j] == 0):
-                TABLEAU[i][j] = TABLEAUBIS[i][j]
+            if(TABLEAU_HORLOGE[i][j] == default[0]):
+                TABLEAU_LEDS[i][j] = TABLEAU_SNOW[i][j]
+            else:
+                TABLEAU_LEDS[i][j] = TABLEAU_HORLOGE[i][j]
 
 
-
-    tableauVersLEDS()
-
-
-def initTableau():
-    global TABLEAU
+def initTableauLeds():
+    global TABLEAU_LEDS
     global default
    
-    TABLEAU = []
+    TABLEAU_LEDS = []
     for i in range(32):
-    	TABLEAU.append([default[0],default[0],default[0],default[0],default[0],default[0],default[0],default[0]])
+    	TABLEAU_LEDS.append([default[0],default[0],default[0],default[0],default[0],default[0],default[0],default[0]])
 
 
-def initTableaubis():
-    global TABLEAUBIS
+def initTableauHorloge():
+    global TABLEAU_HORLOGE
+    global default
+   
+    TABLEAU_HORLOGE = []
+    for i in range(32):
+    	TABLEAU_HORLOGE.append([default[0],default[0],default[0],default[0],default[0],default[0],default[0],default[0]])
+
+
+def initTableauSnow():
+    global TABLEAU_SNOW
     global default
     
-    TABLEAUBIS = []
+    TABLEAU_SNOW = []
     for i in range(32):
-    	TABLEAUBIS.append([default[0],default[0],default[0],default[0],default[0],default[0],default[0],default[0]])
+    	TABLEAU_SNOW.append([default[0],default[0],default[0],default[0],default[0],default[0],default[0],default[0]])
     
     
 def initBackground():
     print("Choosing background...")
-    #num = str(randrange(1,5))
+    num = str(randrange(1,3))
     
-    f = open("/root/Clock-Project/Backgrounds/" + "1" + ".txt", "r")
+    f = open("/root/Clock-Project/Backgrounds/" + num + ".txt", "r")
 
     idx = 0
     for line in f:
@@ -197,8 +213,8 @@ def initBackground():
         line = line[:-1]
         pixel = line.split("|")
         pixel.reverse();
-        for idx2 in reversed(range(len(TABLEAU))):
-            TABLEAU[idx2][idx] = str(pixel[idx2])
+        for idx2 in reversed(range(len(TABLEAU_HORLOGE))):
+            TABLEAU_HORLOGE[idx2][idx] = str(pixel[idx2])
         idx += 1
     f.close()
 
@@ -217,10 +233,10 @@ if(__name__ == '__main__'):
         print("[!] Press ctrl-c to exit")
 
         print("Reinitialisation des tableaux...")
-        initTableau()
-        initTableaubis()
+        initTableauHorloge()
+        initTableauSnow()
+        initTableauLeds()
         horloge()
-        initBackground()
 
         changeHeure = 0
         while True:
@@ -228,15 +244,14 @@ if(__name__ == '__main__'):
             minutesNow = minutes % 10
 
             if(minutesNow != changeHeure):
+                #initBackground()
                 horloge()
-                #if(minutesNow == 0):
-                    #initTableaubis()
                 changeHeure = minutesNow
 
-            #snow()
-            #blinking()
+            snow()
             tableauVersLEDS()
-            time.sleep(1)
+            #blinking()
+            time.sleep(uniform(0.1, 0.4))
 
     except KeyboardInterrupt:
         terminateProcess(0,0)
